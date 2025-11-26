3d67a22511857aa53e95ae669fed945ca03b9f10
acanizares04-alt <acanizares04@gmail.com>
2025-11-26 00:23:16 -0600

    Fix database config file extension in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const connectDB = require('./config/database');
+const connectDB = require('./config/database.js');
 
 const app = express();
 app.use(cors());
