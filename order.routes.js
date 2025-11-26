349d436b984fdf65956c049b5435e6e3112bae13
acanizares04-alt <acanizares04@gmail.com>
2025-11-26 00:34:54 -0600

    Delete order.routes.js

--- order.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/order.controller');
-const auth = require('../middleware/auth');
-
-router.post('/', auth, orderController.createOrder);
-router.get('/', auth, orderController.listOrders);
-
-module.exports = router;
